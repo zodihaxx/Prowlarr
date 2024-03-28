2803ad5ba07427dcb8ea93fd726151059153b17e
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-03-29 01:43:52 +0200

    Update name for RuTracker.org
    Co-authored-by: ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>

--- src/NzbDrone.Core/Indexers/Definitions/RuTracker.cs
+++ src/NzbDrone.Core/Indexers/Definitions/RuTracker.cs
@@ -23,14 +23,14 @@ namespace NzbDrone.Core.Indexers.Definitions
 {
     public class RuTracker : TorrentIndexerBase<RuTrackerSettings>
     {
-        public override string Name => "RuTracker";
+        public override string Name => "RuTracker.org";
         public override string[] IndexerUrls => new[]
         {
             "https://rutracker.org/",
             "https://rutracker.net/",
             "https://rutracker.nl/"
         };
-        public override string Description => "RuTracker is a Semi-Private Russian torrent site with a thriving file-sharing community";
+        public override string Description => "RuTracker.org is a Semi-Private Russian torrent site with a thriving file-sharing community";
         public override string Language => "ru-RU";
         public override Encoding Encoding => Encoding.GetEncoding("windows-1251");
         public override IndexerPrivacy Privacy => IndexerPrivacy.SemiPrivate;
